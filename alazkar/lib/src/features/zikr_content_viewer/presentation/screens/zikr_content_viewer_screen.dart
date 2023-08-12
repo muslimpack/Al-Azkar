02a5fe3c0f2e2b94f24411995881a5bccedb82ed
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-08-12 20:47:38 +0300

    Create zikr_content_viewer_screen.dart

---
+++ alazkar/lib/src/features/zikr_content_viewer/presentation/screens/zikr_content_viewer_screen.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class ZikrContentViewerScreen extends StatelessWidget {
+  final int titleId;
+  const ZikrContentViewerScreen({
+    super.key,
+    required this.titleId,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
