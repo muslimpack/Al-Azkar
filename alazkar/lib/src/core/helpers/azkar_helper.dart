795c9ca55386d68f66d339cc5927309d1f33c665
Hasan Ragab Eltantawy <hasaneltantawy@gmail.com>
2024-06-18 16:40:08 +0300

    update azkar db

--- alazkar/lib/src/core/helpers/azkar_helper.dart
+++ alazkar/lib/src/core/helpers/azkar_helper.dart
@@ -11,7 +11,7 @@ class AzkarDBHelper {
   /* ************* Variables ************* */
 
   static const String dbName = "Al-Azkar.db";
-  static const int dbVersion = 8;
+  static const int dbVersion = 9;
 
   /* ************* Singleton Constructor ************* */
 
