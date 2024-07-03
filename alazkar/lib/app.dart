15fc28becf54eb70a408c007187149404bdc9da8
Hasan Ragab Eltantawy <hasaneltantawy@gmail.com>
2024-07-03 08:42:26 +0300

    Update app.dart

--- alazkar/lib/app.dart
+++ alazkar/lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:alazkar/src/features/home/presentation/controller/home/home_bloc.dart';
 import 'package:alazkar/src/features/home/presentation/screens/home_page_screen.dart';
 import 'package:alazkar/src/features/search/presentation/controller/cubit/search_cubit.dart';
+import 'package:alazkar/src/features/settings/presentation/controller/cubit/settings_cubit.dart';
 import 'package:alazkar/src/features/theme/presentation/controller/cubit/theme_cubit.dart';
 import 'package:alazkar/src/features/zikr_source_filter/presentation/controller/cubit/zikr_source_filter_cubit.dart';
 import 'package:flutter/material.dart';
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
+        BlocProvider(create: (_) => SettingsCubit()),
         BlocProvider(create: (_) => ThemeCubit()),
         BlocProvider(create: (_) => ZikrSourceFilterCubit()..start()),
         BlocProvider(create: (_) => HomeBloc()),
