128f81f68789e0bf784e2ec89cb48ef286c6d0ee
7eltantawy <hasaneltantawy@gmail.com>
2024-12-16 07:00:30 +0200

    update azkar db v12

--- alazkar/lib/src/core/helpers/azkar_helper.dart
+++ alazkar/lib/src/core/helpers/azkar_helper.dart
@@ -12,7 +12,7 @@ class AzkarDBHelper {
   /* ************* Variables ************* */
 
   static const String dbName = "Al-Azkar.db";
-  static const int dbVersion = 11;
+  static const int dbVersion = 12;
 
   /* ************* Singleton Constructor ************* */
 
