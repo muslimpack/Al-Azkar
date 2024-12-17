968a6f395c5b7d09cbedf9865eb1344d153976fe
7eltantawy <hasaneltantawy@gmail.com>
2024-12-17 16:31:52 +0200

    Update share_as_image_screen.dart

--- alazkar/lib/src/features/share_as_image/presentation/screens/share_as_image_screen.dart
+++ alazkar/lib/src/features/share_as_image/presentation/screens/share_as_image_screen.dart
@@ -2,15 +2,12 @@
 import 'package:alazkar/src/core/di/dependency_injection.dart';
 import 'package:alazkar/src/core/models/zikr.dart';
 import 'package:alazkar/src/core/models/zikr_title.dart';
-import 'package:alazkar/src/core/widgets/loading.dart';
-import 'package:alazkar/src/features/share_as_image/presentation/components/app_bar.dart';
-import 'package:alazkar/src/features/share_as_image/presentation/components/image_body.dart';
-import 'package:alazkar/src/features/share_as_image/presentation/components/share_as_image_bottom_bar.dart';
-import 'package:alazkar/src/features/share_as_image/presentation/controller/share_as_image/share_as_image_bloc.dart';
+import 'package:alazkar/src/features/share_as_image/presentation/components/shareable_image_card.dart';
+import 'package:alazkar/src/features/share_as_image/presentation/controller/cubit/share_image_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ShareAsImageScreen extends StatefulWidget {
+class ShareAsImageScreen extends StatelessWidget {
   static const String routeName = "/ShareAsImage";
 
   static Route route({required Zikr zikr, required ZikrTitle zikrTitle}) {
@@ -32,52 +29,78 @@ class ShareAsImageScreen extends StatefulWidget {
   });
 
   @override
-  State<ShareAsImageScreen> createState() => _ShareAsImageScreenState();
-}
-
-class _ShareAsImageScreenState extends State<ShareAsImageScreen> {
-  final DraggableScrollableController draggableScrollableController =
-      DraggableScrollableController();
-  final GlobalKey imageKey = GlobalKey();
-  final GlobalKey repaintKey = GlobalKey();
-  final GlobalKey interactiveViewerKey = GlobalKey();
-
-  @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => sl<ShareAsImageBloc>()
-        ..add(
-          ShareAsImageStartEvent(
-            zikr: widget.zikr,
-            zikrTitle: widget.zikrTitle,
-          ),
-        ),
-      child: BlocBuilder<ShareAsImageBloc, ShareAsImageState>(
+      create: (context) =>
+          sl<ShareImageCubit>()..start(zikr: zikr, zikrTitle: zikrTitle),
+      child: BlocBuilder<ShareImageCubit, ShareImageState>(
         builder: (context, state) {
-          if (state is ShareAsImageLoadingState) {
-            return const Loading();
-          }
-          if (state is ShareAsImageLoadedState) {
-            return Scaffold(
-              resizeToAvoidBottomInset: false,
-              appBar: PreferredSize(
-                preferredSize: const Size.fromHeight(50),
-                child: ShareAsImageAppBar(
-                  state: state,
-                  repaintKey: repaintKey,
-                ),
-              ),
-              body: ShareAsImageBody(
-                state: state,
-                interactiveViewerKey: interactiveViewerKey,
-                imageKey: imageKey,
-                repaintKey: repaintKey,
-              ),
-              bottomSheet: const ShareAsImageBottomBar(),
+          if (state is! ShareImageLoadedState) {
+            return const Center(
+              child: CircularProgressIndicator(),
             );
           }
-
-          return const SizedBox();
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text(
+                "مشاركة كصورة",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Uthmanic",
+                ),
+              ),
+              centerTitle: true,
+              actions: [
+                IconButton(
+                  onPressed: () async {
+                    await context.read<ShareImageCubit>().shareImage(false);
+                  },
+                  icon: const Icon(Icons.share),
+                ),
+              ],
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(10),
+                child: state.showLoadingIndicator
+                    ? const LinearProgressIndicator()
+                    : const SizedBox.shrink(),
+              ),
+            ),
+            body: PageView.builder(
+              controller: context.read<ShareImageCubit>().pageController,
+              itemCount: state.splittedMatn.length,
+              onPageChanged: context.read<ShareImageCubit>().onPageChanged,
+              itemBuilder: (context, index) {
+                return Stack(
+                  fit: StackFit.expand,
+                  alignment: Alignment.center,
+                  children: [
+                    FittedBox(
+                      child: RepaintBoundary(
+                        key: context.read<ShareImageCubit>().imageKeys[index],
+                        child: ShareableImageCard(
+                          zikr: state.zika,
+                          zikrTitle: state.zikrTitle,
+                          settings: state.settings,
+                          matnRange: state.splittedMatn[index],
+                          splittedLength: state.splittedMatn.length,
+                          splittedindex: index,
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+            bottomNavigationBar: state.splittedMatn.length < 2
+                ? null
+                : BottomAppBar(
+                    height: kToolbarHeight,
+                    child: Text(
+                      "${state.splittedMatn.length} : ${state.activeIndex + 1}",
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+          );
         },
       ),
     );
