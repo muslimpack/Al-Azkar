7333a16aacfe0f9b7a692da39620fe750c376426
7eltantawy <hasaneltantawy@gmail.com>
2024-12-18 19:23:40 +0200

    add toARGB32 to extension_color to replace color value

--- alazkar/lib/src/core/extension/extension_color.dart
+++ alazkar/lib/src/core/extension/extension_color.dart
@@ -20,4 +20,23 @@ extension HexColorConvertor on Color {
     final luminance = computeLuminance();
     return luminance > 0.5 ? Colors.black : Colors.white;
   }
+
+  static int floatToInt8(double x) {
+    return (x * 255.0).round() & 0xff;
+  }
+
+  /// A 32 bit value representing this color.
+  ///
+  /// The bits are assigned as follows:
+  ///
+  /// * Bits 24-31 are the alpha value.
+  /// * Bits 16-23 are the red value.
+  /// * Bits 8-15 are the green value.
+  /// * Bits 0-7 are the blue value.
+  int get toARGB32 {
+    return floatToInt8(a) << 24 |
+        floatToInt8(r) << 16 |
+        floatToInt8(g) << 8 |
+        floatToInt8(b) << 0;
+  }
 }
