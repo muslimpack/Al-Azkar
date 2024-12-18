4153de0c6c82248cacaf0658c1ede49b4e1029ab
7eltantawy <hasaneltantawy@gmail.com>
2024-12-18 12:42:40 +0200

    Update settings_screen.dart

--- alazkar/lib/src/features/settings/presentation/screens/settings_screen.dart
+++ alazkar/lib/src/features/settings/presentation/screens/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:alazkar/src/features/about/presentation/screens/about_screen.dart';
 import 'package:alazkar/src/features/settings/presentation/components/praise_with_volume_switch.dart';
+import 'package:alazkar/src/features/settings/presentation/components/settings_group_title.dart';
 import 'package:alazkar/src/features/settings/presentation/components/show_text_in_bracket_switch.dart';
 import 'package:alazkar/src/features/theme/presentation/screens/theme_screen.dart';
 import 'package:alazkar/src/features/zikr_source_filter/presentation/screens/select_zikr_hokm_screen.dart';
@@ -19,6 +20,7 @@ class SettingsScreen extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(15),
         children: [
+          const SettingsGroupTitle(title: "عام"),
           ListTile(
             leading: const Icon(Icons.palette),
             title: const Text("مظهر التطبيق"),
@@ -35,6 +37,7 @@ class SettingsScreen extends StatelessWidget {
           const ShowTextInBracketsSwitch(),
           const PraiseWithVolumeSwitch(),
           const Divider(),
+          const SettingsGroupTitle(title: "تصفية الأذكار"),
           ListTile(
             leading: const Icon(Icons.library_books),
             title: const Text("اختيار مصدر الأذكار"),
@@ -62,6 +65,7 @@ class SettingsScreen extends StatelessWidget {
             },
           ),
           const Divider(),
+          const SettingsGroupTitle(title: "حول التطبيق"),
           ListTile(
             leading: const Icon(Icons.info),
             title: const Text("عنا"),
