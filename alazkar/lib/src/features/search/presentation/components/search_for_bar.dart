e3487556f94607cb5d7e92268615673644208740
7Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2025-05-31 06:36:56 +0300

    Create search_for_bar.dart

---
+++ alazkar/lib/src/features/search/presentation/components/search_for_bar.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'package:alazkar/src/features/search/data/models/search_for.dart';
+import 'package:alazkar/src/features/search/presentation/controller/cubit/search_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SearchForBar extends StatelessWidget {
+  const SearchForBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SearchCubit, SearchState>(
+      builder: (context, state) {
+        if (state is! SearchLoadedState) return const SizedBox.shrink();
+        return Wrap(
+          alignment: WrapAlignment.center,
+          runSpacing: 10,
+          spacing: 10,
+          children: SearchFor.values.map((e) {
+            return ChoiceChip(
+              label: Text(
+                e.localeName(context),
+              ),
+              showCheckmark: false,
+              selected: state.searchFor == e,
+              onSelected: (value) async {
+                await context.read<SearchCubit>().changeSearchFor(e);
+              },
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+}
